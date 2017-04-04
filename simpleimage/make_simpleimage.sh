f30798b5c3d14e6c872520172d311b219b65a72d
Ben Tinner <bentinner@yahoo.com.sg>
2017-04-04 16:11:50 +0800

    Added the ability to generate different types of simpleimage for different configurations
    Signed-off-by: Ben Tinner <bentinner@yahoo.com.sg>

--- simpleimage/make_simpleimage.sh
+++ simpleimage/make_simpleimage.sh
@@ -17,12 +17,23 @@
 
 set -e
 
-out="$1"
-disk_size="$2"
-kernel_tarball="$3"
+type="$1"
+out="$2"
+disk_size="$3"
+kernel_tarball="$4"
 
 if [ -z "$out" ]; then
-	echo "Usage: $0 <image-file.img> [disk size in MiB] [<kernel-tarball>]"
+	echo "Usage: $0 <image-type> <image-file.img> [disk size in MiB] [kernel-tarball]"
+	echo ""
+	echo "Image type:"
+	echo ""
+	echo "pine64      - Pine A64"
+	echo "pine64lcd   - Pine A64 with LCD"
+	echo "sopine      - SoPine A64"
+	echo "pinebook    - Pinebook"
+	echo ""
+	exit 1
+fi
 	exit 1
 fi
 
@@ -35,9 +46,19 @@ if [ "$disk_size" -lt 60 ]; then
 	exit 2
 fi
 
+if [ $type = "pine64" -o $type = "pine64lcd" ]; then
+	boot0="../blobs/boot0.bin"
+elif [ $type = "sopine" ]; then
+	boot0="../blobs/boot0so.bin"
+elif [ $type = "pinebook" ]; then
+	boot0="../blobs/boot0book.bin"
+else
+	echo "Invalid image type specified"
+	exit 3
+fi
+
 echo "Creating image $out of size $disk_size MiB ..."
 
-boot0="../blobs/boot0.bin"
 uboot="../build/u-boot-with-dtb.bin"
 kernel="../build"
 
@@ -55,6 +76,14 @@ if [ -n "$kernel_tarball" ]; then
 	tar -C $temp -xJf "$kernel_tarball"
 	kernel=$temp/boot
 	mv $temp/boot/uEnv.txt.in $temp/boot/uEnv.txt
+fi
+
+if [ $type = "pine64lcd" ]; then
+	echo "pine64_model=pine64-plus-lcd" >> $temp/boot/uEnv.txt
+elif [ $type = "sopine" ]; then
+	echo "pine64_model=pine64-so" >> $temp/boot/uEnv.txt
+elif [ $type = "pinebook" ]; then
+	echo "pine64_model=pinebook" >> $temp/boot/uEnv.txt
 fi
 
 boot0_position=8      # KiB
